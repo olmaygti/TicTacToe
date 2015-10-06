c8e382dd0b371a0fed4ffe69aed66815754a8448
olmaygti <dortega@twoapart.com>
2015-10-06 12:26:32 +0200

    Created (hardcoded) players in the scope, created a stupied move function to switch between them

--- grails-app/assets/javascripts/ticTac.js
+++ grails-app/assets/javascripts/ticTac.js
@@ -8,15 +8,39 @@
             function ($scope, Player, Board) {
             var ctx, board;
 
-            console.log('controller wired');
-
             function init () {
                 $scope.ctx = ctx = {};
+            }
+
+            function nextPlayer() {
+                return ctx.currentPlayer === ctx.players[0]
+                    ? ctx.players[1]
+                    : ctx.players[0];
+            }
+
+            $scope.move = function (cell) {
+
+                console.log((ctx.currentPlayer.name) + ' moved to ' + cell);
+
+                console.log('Next player is: ', nextPlayer());
             }
 
             $scope.startGame = function () {
                 ctx.board = board = new Board();
                 board.initBoard();
+                ctx.players = [
+                    new Player({
+                        human: true,
+                        style: 'circle',
+                        name: 'David'
+                    }),
+                    new Player({
+                        human: true,
+                        style: 'cross',
+                        name: 'Daniel'
+                    })
+                ];
+                ctx.currentPlayer = ctx.players[0];
             }
             init();
         }]);
